9adbda83ba439df3aa91d5488956e7f09e77af65
brombaut <ben.rombaut@unb.ca>
2021-04-18 21:11:44 -0400

    0.0.4

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -1,6 +1,4 @@
-export * from './FirestoreBase'
-export * from './FirestoreCollection'
-export * from './FirestoreDate'
-export * from './DateTranslator'
-
-
+export * from './FirestoreBase';
+export * from './FirestoreCollection';
+export * from './FirestoreDate';
+export * from './DateTranslator';
