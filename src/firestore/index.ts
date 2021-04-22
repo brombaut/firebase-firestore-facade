8c28dfca8b9f5df8496d98e63e10820e8f540a2b
brombaut <ben.rombaut@unb.ca>
2021-04-22 16:30:58 -0400

    Exporting all modules

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -1,7 +1,11 @@
 // TODO: Remove these unneeded ones
+export * from './F3Interfacer';
+export * from './F3Wrapper';
+export * from './FirebaseConfigurer';
 export * from './FirestoreBase';
 export * from './FirestoreCollection';
 export * from './FirestoreDate';
 export * from './FirestoreDateTranslator';
-export * from './F3Interfacer';
-export * from './F3Wrapper';
+export * from './IFirestoreType';
+export * from './ILocalType';
+
