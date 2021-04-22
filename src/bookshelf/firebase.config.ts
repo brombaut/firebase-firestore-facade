5be17cc8950f07a45421bc9e51893baf2ece69bb
brombaut <ben.rombaut@unb.ca>
2021-04-22 18:00:52 -0400

    Removing firebase config from ignore

---
+++ src/bookshelf/firebase.config.ts
@@ -0,0 +1,15 @@
+import dotenv = require('dotenv');
+import { FirebaseConfigurer } from '../firestore/FirebaseConfigurer';
+dotenv.config();
+
+const firebaseConfig: FirebaseConfigurer = {
+  apiKey: process.env.API_KEY || '',
+  authDomain: process.env.AUTH_DOMAIN || '',
+  projectId: process.env.PROJECT_ID || '',
+  storageBucket: process.env.STORAGE_BUCKET || '',
+  messagingSenderId: process.env.MESSAGING_SENDER_ID || '',
+  appId: process.env.APP_ID || '',
+  measurementId: process.env.MEASUREMENT_ID || '',
+};
+
+export { firebaseConfig };
